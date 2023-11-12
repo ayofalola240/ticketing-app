1fe48b0d1ada41c76f4d3c86f26f570ad015f0c9
ayofalola240 <ayofalola240@gmail.com>
2023-11-12 23:10:10 +0100

    implement signin

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -5,7 +5,6 @@ import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
-// import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
