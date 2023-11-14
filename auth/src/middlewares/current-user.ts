e75ddd29e6259e0280eba8bae2f5554e9eb4ed7b
ayofalola240 <ayofalola240@gmail.com>
2023-11-14 11:30:14 +0100

    new update

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -14,20 +14,13 @@ declare global {
   }
 }
 
-export const currentUser = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const currentUser = (req: Request, res: Response, next: NextFunction) => {
   if (!req.session?.jwt) {
     return next();
   }
 
   try {
-    const payload = jwt.verify(
-      req.session.jwt,
-      process.env.JWT_KEY!
-    ) as UserPayload;
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
     req.currentUser = payload;
   } catch (err) {}
 
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -3,18 +3,18 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
 
-// declare global {
-//   namespace NodeJS {
-//     interface Global {
-//       signin(): Promise<string[]>;
-//     }
-//   }
-// }
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdfasdf';
-  // process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
   mongo = await MongoMemoryServer.create();
   const mongoUri = await mongo.getUri();
@@ -35,19 +35,19 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-// global.signin = async () => {
-//   const email = 'test@test.com';
-//   const password = 'password';
+export const signin = async (): Promise<string[]> => {
+  const email = 'test@test.com';
+  const password = 'password';
 
-//   const response = await request(app)
-//     .post('/api/users/signup')
-//     .send({
-//       email,
-//       password,
-//     })
-//     .expect(201);
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
 
-//   const cookie = response.get('Set-Cookie');
+  const cookie = response.get('Set-Cookie');
 
-//   return cookie;
-// };
+  return cookie;
+};
